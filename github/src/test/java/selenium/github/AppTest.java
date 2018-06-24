b7854acac26482b8976e82ffb359ff1971faaa8a
NaveenAutomate <naveenavula1989@gmail.com>
2018-06-24 22:44:41 +0530

    initial commit

---
+++ github/src/test/java/selenium/github/AppTest.java
@@ -0,0 +1,16 @@
+package selenium.github;
+
+import org.testng.annotations.Test;
+
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+   
+	@Test
+	public void firstTestCase(){
+		System.out.println("Test case through maven");
+	}
+}
