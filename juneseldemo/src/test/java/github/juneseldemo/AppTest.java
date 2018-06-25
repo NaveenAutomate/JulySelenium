655e9a94e597eaf72ce30046317799e185c72537
NaveenAutomate <naveenavula1989@gmail.com>
2018-06-25 20:59:58 +0530

    Demo for github

---
+++ juneseldemo/src/test/java/github/juneseldemo/AppTest.java
@@ -0,0 +1,17 @@
+package github.juneseldemo;
+
+import org.testng.annotations.Test;
+
+
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest
+{
+	
+	@Test
+	public void firstTestCAse(){
+		System.out.println("this is first test case");
+	}
+}
