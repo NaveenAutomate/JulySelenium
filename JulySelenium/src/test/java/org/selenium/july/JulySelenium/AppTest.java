14832288d1613575baec1b2901ac994dbba55d61
NaveenAutomate <naveenavula1989@gmail.com>
2018-07-24 08:13:24 +0530

    initial commit of workspace to repo

---
+++ JulySelenium/src/test/java/org/selenium/july/JulySelenium/AppTest.java
@@ -0,0 +1,17 @@
+package org.selenium.july.JulySelenium;
+
+import org.testng.annotations.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest
+
+{
+	
+	@Test
+	public void firstTest(){
+		
+		System.out.println("This is a test using maven ");
+	}
+}
--- juneseldemo/src/test/java/github/juneseldemo/AppTest.java
+++ juneseldemo/src/test/java/github/juneseldemo/AppTest.java
@@ -14,4 +14,16 @@ public class AppTest
 	public void firstTestCAse(){
 		System.out.println("this is first test case");
 	}
+	
+	//Second persion -- v1-->v3--v4
+	@Test
+	public void secondTestCase(){
+		
+	}
+	
+	//Third person - v1-->v2
+	@Test
+	public void thirdTestCase(){
+		
+	}
 }
